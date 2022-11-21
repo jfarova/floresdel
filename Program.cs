8c58f96e4ad3b2a35fbf77d8d0680b92df414fa0
chenaoh <jfarova112333@gmail.com>
2022-11-20 21:23:11 -0500

    Programa que lea una serie de números (leer hasta que llegue el cero por ejemplo) y que calcule su media aritmética

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num=1,suma=0;
+       while(num!=0){
+        Console.WriteLine("ingrese numero");
+        num=int.Parse(Console.ReadLine());
+        suma=suma+(1/num);
+       }
+
+       Console.WriteLine("la media aritmetica es : " +suma);
+    }
+}
